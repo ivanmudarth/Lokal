05c82d24cbdb339ba06d39c9c7f11ae6c5732519
Ivan Mudarth <ivan.mudarth@yahoo.com>
2021-02-24 00:46:11 -0500

    add filter button

--- lokal/lib/home.dart
+++ lokal/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -15,25 +16,53 @@ class HomePage extends StatelessWidget {
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Container(
+              child: SvgPicture.asset("assets/images/filter_icon.svg"),
+            ),
             SizedBox(height: 70),
             Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 55.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    Row(
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: 'Discover ',
+                            style: TextStyle(fontSize: 36, color: Colors.black),
+                          ),
+                        ),
+                        SizedBox(width: 40),
+                        MaterialButton(
+                          elevation: 1.0,
+                          padding: EdgeInsets.all(12.0),
+                          textColor: Colors.white,
+                          color: Colors.white,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/images/filter_icon.png'),
+                                  fit: BoxFit.contain),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(13.0),
+                            ),
+                          ),
+                          shape: CircleBorder(),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
                     RichText(
                       text: TextSpan(
-                        text: 'Discover ',
-                        style: TextStyle(fontSize: 36, color: Colors.black),
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: 'Restaurants',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                decoration: TextDecoration.underline,
-                                color: Color(0xff96BE4B)),
-                          ),
-                        ],
+                        text: 'Restaurants',
+                        style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                            color: Color(0xff96BE4B)),
                       ),
                     ),
                     SizedBox(height: 10),
