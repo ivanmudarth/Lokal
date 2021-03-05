741aeeb3c697260ff3a36f2336f8acdebc542309
Ivan Mudarth <ivan.mudarth@yahoo.com>
2021-03-05 04:12:43 -0500

    start profile

--- lokal/lib/main.dart
+++ lokal/lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import './home.dart';
 // ignore: unused_import
 import './search.dart';
+// ignore: unused_import
 import './biz/home.dart';
+// ignore: unused_import
+import './biz/profile.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,11 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lokal App',
-      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: ""),
-      //home: HomePage(), // uncomment to display page
-      //home: SearchPage(), // uncomment to display page
-      home: BizHome(),
-    );
+        title: 'Lokal App',
+        theme: ThemeData(primarySwatch: Colors.blue, fontFamily: ""),
+        //home: HomePage(), // uncomment to display page
+        //home: SearchPage(), // uncomment to display page
+        // home: BizHome(), // uncomment to display page
+        home: Profile());
   }
 }
