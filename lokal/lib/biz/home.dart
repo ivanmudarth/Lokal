1f9f6d6432f31d3b8b40cb7586809b011c47cb6f
Ivan Mudarth <ivan.mudarth@yahoo.com>
2021-03-05 03:19:56 -0500

    add carousel

--- lokal/lib/biz/home.dart
+++ lokal/lib/biz/home.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class BizHome extends StatelessWidget {
+// ignore: must_be_immutable
+class BizHome extends StatefulWidget {
   @override
+  _BizHomeState createState() => _BizHomeState();
+}
+
+class _BizHomeState extends State<BizHome> {
+  var _current = 0;
+  var list = [
+    'Add in promotions, menus and flyers',
+    'add text 1',
+    'add text 2'
+  ];
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffE5E5E5),
@@ -14,10 +26,9 @@ class BizHome extends StatelessWidget {
     return ListView(
       children: [
         Column(
-          mainAxisSize: MainAxisSize.max,
           children: [
             SizedBox(
-              height: 600,
+              height: 620,
               width: size.width,
               child: Container(
                 decoration: BoxDecoration(
@@ -65,15 +76,85 @@ class BizHome extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 70),
-            SizedBox(height: 40),
+            SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 25,
+                  height: 25,
+                  child: Image(image: AssetImage("assets/images/star.png")),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: "Stand out with a customized profile",
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+              ],
+            ),
+            Container(
+                child: Column(children: [
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 60,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  },
+                ),
+                items: list
+                    .map((item) => Container(
+                          child: Center(
+                              child: Text(
+                            item.toString(),
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          )),
+                        ))
+                    .toList(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: list.map(
+                  (item) {
+                    int index = list.indexOf(item);
+                    return Container(
+                      width: 8.0,
+                      height: 8.0,
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _current == index
+                              ? Color.fromRGBO(0, 0, 0, 0.9)
+                              : Color.fromRGBO(0, 0, 0, 0.4)),
+                      child: SizedBox(
+                        width: 50,
+                      ),
+                    );
+                  },
+                ).toList(),
+              )
+            ]))
           ],
         )
       ],
     );
   }
 
-  // ignore: non_constant_identifier_names
+// ignore: non_constant_identifier_names
   Widget CategoryButton({String text}) {
     return ButtonTheme(
         height: 40,
