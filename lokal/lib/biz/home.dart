e2c4c0567fd5ee7c81f27f9462cd85a16eaa24e7
Ivan Mudarth <ivan.mudarth@yahoo.com>
2021-03-05 23:12:32 -0500

    fix home

--- lokal/lib/biz/home.dart
+++ lokal/lib/biz/home.dart
@@ -16,7 +16,7 @@ class _BizHomeState extends State<BizHome> {
   ];
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffE5E5E5),
+      backgroundColor: Color(0xffFFFFFF),
       body: getBody(context),
     );
   }
@@ -28,137 +28,144 @@ class _BizHomeState extends State<BizHome> {
         Column(
           children: [
             SizedBox(
+              height: 75,
+            ),
+            Container(
+              width: 300,
+              height: 300,
+              child: Image(image: AssetImage("assets/images/bizlogo.png")),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            RichText(
+              text: TextSpan(
+                text: "Kid to Kid",
+                style: TextStyle(
+                    fontSize: 36,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                BigButton(text: "Settings", img: "settings"),
+                SizedBox(
+                  width: 20,
+                ),
+                BigButton(text: "Edit Profile", img: "edit"),
+              ],
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            SizedBox(
               height: 620,
               width: size.width,
               child: Container(
                 decoration: BoxDecoration(
-                    color: Color(0xffFFFFFF),
-                    border: Border.all(color: Colors.grey, width: 3),
-                    borderRadius:
-                        BorderRadius.all(Radius.elliptical(300, 100))),
+                    color: Color(0xffE5E5E5),
+                    border: Border.all(color: Colors.grey, width: 0),
+                    borderRadius: BorderRadius.all(Radius.zero)),
                 child: Column(
                   children: [
-                    SizedBox(
-                      height: 75,
-                    ),
-                    Container(
-                      width: 300,
-                      height: 300,
-                      child:
-                          Image(image: AssetImage("assets/images/bizlogo.png")),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    RichText(
-                      text: TextSpan(
-                        text: "Mangi's Italiano",
-                        style: TextStyle(
-                            fontSize: 36,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
+                    // ADD
+                    SizedBox(height: 15),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        BigButton(text: "Settings", img: "settings"),
-                        SizedBox(
-                          width: 20,
+                        Container(
+                          width: 25,
+                          height: 25,
+                          child: Image(
+                              image: AssetImage("assets/images/star.png")),
                         ),
-                        BigButton(text: "Edit Profile", img: "edit"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            text: "Stand out with a customized profile",
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
                       ],
-                    )
+                    ),
+                    Container(
+                        child: Column(children: [
+                      CarouselSlider(
+                        options: CarouselOptions(
+                          height: 50,
+                          onPageChanged: (index, reason) {
+                            setState(() {
+                              _current = index;
+                            });
+                          },
+                        ),
+                        items: list
+                            .map((item) => Container(
+                                  child: Center(
+                                      child: Text(
+                                    item.toString(),
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                    ),
+                                  )),
+                                ))
+                            .toList(),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: list.map(
+                          (item) {
+                            int index = list.indexOf(item);
+                            return Container(
+                              width: 8.0,
+                              height: 8.0,
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 10.0, horizontal: 2.0),
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: _current == index
+                                      ? Color.fromRGBO(0, 0, 0, 0.9)
+                                      : Color.fromRGBO(0, 0, 0, 0.4)),
+                            );
+                          },
+                        ).toList(),
+                      )
+                    ])),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    ButtonTheme(
+                      height: 40,
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: Color(0xffAFD755))),
+                        onPressed: () {},
+                        elevation: 5,
+                        color: Color(0xffAFD755),
+                        textColor: Colors.white,
+                        child:
+                            Text(" My Lokal+ ", style: TextStyle(fontSize: 17)),
+                      ),
+                    ),
                   ],
                 ),
               ),
             ),
-            SizedBox(height: 15),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 25,
-                  height: 25,
-                  child: Image(image: AssetImage("assets/images/star.png")),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                RichText(
-                  text: TextSpan(
-                    text: "Stand out with a customized profile",
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-              ],
-            ),
-            Container(
-                child: Column(children: [
-              CarouselSlider(
-                options: CarouselOptions(
-                  height: 50,
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      _current = index;
-                    });
-                  },
-                ),
-                items: list
-                    .map((item) => Container(
-                          child: Center(
-                              child: Text(
-                            item.toString(),
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
-                          )),
-                        ))
-                    .toList(),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: list.map(
-                  (item) {
-                    int index = list.indexOf(item);
-                    return Container(
-                      width: 8.0,
-                      height: 8.0,
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: _current == index
-                              ? Color.fromRGBO(0, 0, 0, 0.9)
-                              : Color.fromRGBO(0, 0, 0, 0.4)),
-                    );
-                  },
-                ).toList(),
-              )
-            ])),
-            ButtonTheme(
-              height: 40,
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                    side: BorderSide(color: Color(0xffAFD755))),
-                onPressed: () {},
-                elevation: 5,
-                color: Color(0xffAFD755),
-                textColor: Colors.white,
-                child: Text(" My Lokal+ ", style: TextStyle(fontSize: 17)),
-              ),
-            ),
-          ],
+          ], // end here
         )
       ],
     );
--- lokal/lib/main.dart
+++ lokal/lib/main.dart
@@ -17,11 +17,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Lokal App',
-        theme: ThemeData(primarySwatch: Colors.blue, fontFamily: ""),
-        //home: HomePage(), // uncomment to display page
-        //home: SearchPage(), // uncomment to display page
-        // home: BizHome(), // uncomment to display page
-        home: Metrics());
+      title: 'Lokal App',
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: ""),
+      //home: HomePage(), // uncomment to display page
+      //home: SearchPage(), // uncomment to display page
+      home: BizHome(),
+      //home: Profile(),
+    ); // uncomment to display page
+    // home: Metrics());
   }
 }
